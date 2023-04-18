deaeb88b8e7f193609e59b4186e41169332a55ca
dzevakov <dmitryzev@gmail.com>
2023-04-18 10:00:12 +0300

    task-5 integration with s3

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   products: "https://8jduv2iljh.execute-api.us-east-1.amazonaws.com",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://797yocetbg.execute-api.us-east-1.amazonaws.com/",
   bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
